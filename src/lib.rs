dd14fbf00eaefd138416f90d7912d541ee3158ec
Colin Davidson <Colin.Davidson@Siegetechnologies.com>
2015-06-05 14:58:48 -0400

    Add abstract types.

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,36 @@ extern crate byteorder;
 
 pub mod pe;
 pub mod elf;
+
+#[derive(Clone, Copy)]
+pub enum Arch {
+    X86(Width),
+    ARM(Width, Endian, ARMMode, ARMType),
+    PPC(Width, Endian),
+}
+
+#[derive(Clone, Copy)]
+pub enum Endian {
+    Little,
+    Big,
+}
+
+#[derive(Clone, Copy)]
+pub enum Width {
+    W16,
+    W32,
+    W64,
+}
+
+#[derive(Clone, Copy)]
+pub enum ARMMode {
+    ARM,
+    Thumb,
+}
+
+#[derive(Clone, Copy)]
+pub enum ARMType {
+    ARM,
+    MClass,
+    V8,
+}
